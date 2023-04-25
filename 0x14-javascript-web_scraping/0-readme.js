7ab735934525fae6f6427d55602d643c4244479a
peter njihia <peternjihia2001@gmail.com>
2023-04-25 21:44:26 +0000

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-
-#a script that reads and prints the content of a file
-
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+fs.readFile(process.argv[2], 'utf8', function (error, object) {
+  console.log(error || object);
 });
