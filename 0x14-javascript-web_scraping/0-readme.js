03e6d7b442800491193b09be326ac2732560b624
peter njihia <peternjihia2001@gmail.com>
2023-04-25 21:31:15 +0000

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
+""" a script that reads and prints the content of a file. The first argument is the file path"""
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
