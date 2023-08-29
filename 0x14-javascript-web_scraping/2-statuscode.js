cb603d8493089b2bc01169c5ea14676c14b5ef2f
peter njihia <peternjihia2001@gmail.com>
2023-08-29 02:20:02 -0700

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,19 +1,14 @@
+#!/usr/bin/node
+
 const request = require('request');
+//import the request module
 
-// Check if the URL argument is provided
-if (process.argv.length < 3) {
-  console.log('Usage: node 2-statuscode.js <URL>');
-  process.exit(1);
-}
+request.get(process.argv[2])
+//use a request module to perform a get request to the url
 
-// Get the URL from the command line argument
-const url = process.argv[2];
+    .on('response', function (response) {
+        //set up an event listener for the response event emmitted by http request
 
-// Make a GET request to the specified URL
-request.get(url, (err, response) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code:', response.statusCode);
-  }
+        console.log(`code: ${response.statusCode}`);
+        //log the http statuscode of the response to the console.
 });
