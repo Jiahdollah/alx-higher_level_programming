5fcfda0478ab15c59d7f6837cab8293e87abce0f
peter njihia <peternjihia2001@gmail.com>
2023-08-29 01:37:06 -0700

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-#script that writes a string to a file
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+//import the biult in node js fs module
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8',  error => {
+//use fs.writer to write data to a file specified to (process.argv[2]
+//thw data to written is taken from (process.argv[3]
+
+  if (error) 
+     console.log(error);
 });
